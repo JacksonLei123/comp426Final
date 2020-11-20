9eb7f40e5bef26d6f3c115fc7238dd571f3071e1
Kelly Yu <kellyyu@Kellys-MacBook.lan>
2020-11-20 03:59:32 -0500

    hehehe

---
+++ data.js
@@ -0,0 +1,32 @@
+var results = [
+    {
+        location: "Paris",
+        description: "Home of the Eiffel Tower, Louvre, and Arc de Triomphe. Sit down at a cafe, grab a croissant, and maybe even fall in love."
+    },
+    {
+        location: "New Zealand",
+        description: "From beautiful landscapes at mountains to glaciers, New Zealand brims with delicious food and age-old wine."
+    },
+    {
+        location: "Bora Bora",
+        description: "A beautiful, small and secluded island away from the business of the cities. Read a book by the beach, explore the jungles, or climb the island's dormant volcano, Mount Otemanu!"
+    },
+    {
+        location: "London",
+        description: "A blend of historical landmarks and modern tourist attractions, London will capture and attention and keep it. Grab some tea and pay a visit to the Tower of London!"
+    },
+    {
+        location: "Tokyo",
+        description: "Step foot into this cosmopolitan capital, known for its bustling streets and vibrant nightlife. Grab some ramen and sweep the skylines!"
+    },
+    {
+        location: "Phuket",
+        description: "Enjoy the gorgeous white sand beaches and temples of during the day, and the bustling nightlife and boat tours at night."
+    },
+    {
+        location: "Grand Canyon",
+        description: "Roughly 277 miles long, the Grand Canyon national park offers long hikes and whitewater rafting for anyone looking for an outdoor adventure."
+    },
+
+]
+    
