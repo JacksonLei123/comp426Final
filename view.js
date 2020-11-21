dbafdc0e62fc9a50ef4e6551e7e29b20eed7119f
Brianna Li <brili24@live.unc.edu>
2020-11-21 18:42:49 -0500

    push

--- view.js
+++ view.js
@@ -33,7 +33,6 @@ export default class AppView {
             // original user div
             
             let there = that;
-
             let userdiv = `<div>
             <nav>
               <ul>
