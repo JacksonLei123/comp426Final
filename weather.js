f65160bada24de531e6f74e7b7ccbd8f4e0441b1
Janhvi <janhvikulkarni1@Janhvis-MacBook-Pro.local>
2020-11-21 22:44:04 -0500

    weather shows init

--- weather.js
+++ weather.js
@@ -34,16 +34,32 @@ $(document).ready(() => {
             const curr = auth.currentUser;
             console.log(curr.email);
           //location = db.collection("users").doc(curr.email).data.location;
-            let docRef = db.collection("users").doc(curr.email);
+            
+            
+      
+        } else {
+       //     controller.signOut();
+           // $('.user').replaceWith(x);
+            alert("No active user");
+           
+        }
+    
+      })
+      
+})
+
+export async function initialWeather() {
+    auth.onAuthStateChanged(function(user) {
+        if (user) {
+            const curr = auth.currentUser;
+          let docRef = db.collection("users").doc(curr.email);
             docRef.get().then(function(doc) {
             
                 if (doc.exists) {
                  console.log("Document data:", doc.data());
                     
                     location = doc.data().location;
-                 //   console.log(location);
                     let split = location.split("/");
-                    console.log(split[0]);
                     initialCity = split[0];
                     
                 } else {
@@ -53,32 +69,32 @@ $(document).ready(() => {
                 
             }).then(async function() { 
                 console.log(initialCity);
-    const response = await axios({
-        method: 'GET',
-        url: 'https://community-open-weather-map.p.rapidapi.com/forecast/daily',
-        params: {q: initialCity, cnt: '10', units: 'imperial'},
-        headers: {
-            'x-rapidapi-key': '77a0f8f416mshc4af7c3543d5f9bp1a812fjsn303daa15f8cc',
-            'x-rapidapi-host': 'community-open-weather-map.p.rapidapi.com'
-        }
-    });
-    var cityName = response.data.city.name;
-    var country = response.data.city.country;
-    $('#weatherTitle').append(`<h1 class="title is-2"><center>10-Day Weather - ${cityName}, ${country}</center></h1><br>`);
-    for (var i = 0; i < 5; i++) {
-        let weath = response.data.list[i].weather[0].description;
-        let min = "Low: " + response.data.list[i].temp.min + "°F";
-        let max = "High: " + response.data.list[i].temp.max + "°F";
-        let humidity = "Humidity: " + response.data.list[i].humidity + "%";
-        $('#weather1').append(`<div class="column weatherDay"><center>
-        <h1 class="title is-3">DAY ${i+1}</h1><br>
-        <div class="is-size-5">
-        ${weath}<br>
-        ${min}<br>
-        ${max}<br>
-        ${humidity}
-        </div>
-        </center></div>`);
+                const response = await axios({
+                    method: 'GET',
+                    url: 'https://community-open-weather-map.p.rapidapi.com/forecast/daily',
+                    params: {q: initialCity, cnt: '10', units: 'imperial'},
+                    headers: {
+                        'x-rapidapi-key': '77a0f8f416mshc4af7c3543d5f9bp1a812fjsn303daa15f8cc',
+                        'x-rapidapi-host': 'community-open-weather-map.p.rapidapi.com'
+                    }
+                });
+                var cityName = response.data.city.name;
+                var country = response.data.city.country;
+                $('#weatherTitle').append(`<h1 class="title is-2"><center>10-Day Weather - ${cityName}, ${country}</center></h1><br>`);
+                for (var i = 0; i < 5; i++) {
+                    let weath = response.data.list[i].weather[0].description;
+                    let min = "Low: " + response.data.list[i].temp.min + "°F";
+                    let max = "High: " + response.data.list[i].temp.max + "°F";
+                    let humidity = "Humidity: " + response.data.list[i].humidity + "%";
+                    $('#weather1').append(`<div class="column weatherDay"><center>
+                    <h1 class="title is-3">DAY ${i+1}</h1><br>
+                    <div class="is-size-5">
+                    ${weath}<br>
+                    ${min}<br>
+                    ${max}<br>
+                    ${humidity}
+                    </div>
+            </center></div>`);
     }
 
     for (var i = 5; i < 10; i++) {
@@ -98,6 +114,7 @@ $(document).ready(() => {
     }
             });
             
+            
       
         } else {
        //     controller.signOut();
@@ -107,8 +124,8 @@ $(document).ready(() => {
         }
     
       })
-      
-})
+    
+}
 
 export async function getWeather() {
    // console.log("hey");
